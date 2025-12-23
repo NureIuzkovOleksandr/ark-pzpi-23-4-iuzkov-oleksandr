e440c18525ae72920127ece72c5438f51b3b68ce
Yuzkov Oleksandr <oleksandr.iuzkov@nure.ua>
2025-12-23 01:03:15 +0100

    Add files via upload

--- Pract2/ark-pzpi-23-4-iuzkov-oleksandr-pract2/code-examples-refactoring-methods.r
+++ Pract2/ark-pzpi-23-4-iuzkov-oleksandr-pract2/code-examples-refactoring-methods.r
@@ -1,21 +1,84 @@
-#Приклад 1.1
+#МЕТОД EXTRACT METHOD
 #До рефакторингу
-generate_report <- function(data) {
-  cat("Generating report...\n")
-
-  sum_value <- 0
-  for (x in data) {
-    sum_value <- sum_value + x
+analyze_sales <- function(data) {
+  filtered <- data[data$price > 100, ]
+  
+  total <- 0
+  for (i in 1:nrow(filtered)) {
+    total <- total + filtered$price[i]
   }
-  cat("Total sum:", sum_value, "\n")
+  
+  average <- total / nrow(filtered)
+  
+  cat("Total sales:", total, "\n")
+  cat("Average price:", average, "\n")
+}
 
-  avg_value <- 0
-  for (x in data) {
-    avg_value <- avg_value + x
+#Після рефакторингу
+filter_expensive_sales <- function(data) {
+  data[data$price > 100, ]
+}
+
+calculate_total <- function(values) {
+  sum(values)
+}
+
+calculate_average <- function(values) {
+  mean(values)
+}
+
+analyze_sales <- function(data) {
+  filtered <- filter_expensive_sales(data)
+  
+  total <- calculate_total(filtered$price)
+  average <- calculate_average(filtered$price)
+  
+  cat("Total sales:", total, "\n")
+  cat("Average price:", average, "\n")
+}
+#МЕТОД RENAME METHOD
+#До рефакторингу
+calc <- function(a, b, c) {
+  if (b > c) {
+    a * 0.8
+  } else {
+    a * 0.95
   }
-  avg_value <- avg_value / length(data)
-  cat("Average:", avg_value, "\n")
-
-  cat("Report generated.\n")
 }
 #Після рефакторингу
+calculate_discounted_price <- function(price, order_count, discount_threshold) {
+  if (order_count > discount_threshold) {
+    price * 0.8
+  } else {
+    price * 0.95
+  }
+}
+#МЕТОД REPLACE CONDITIONAL WITH POLYMORPHISM
+#До рефакторингу
+calculate_tax <- function(type, income) {
+  if (type == "student") {
+    income * 0.05
+  } else if (type == "employee") {
+    income * 0.18
+  } else if (type == "business") {
+    income * 0.25
+  } else {
+    stop("Unknown taxpayer type")
+  }
+}
+#Після рефакторингу
+calculate_tax <- function(person, income) {
+  UseMethod("calculate_tax")
+}
+
+calculate_tax.student <- function(person, income) {
+  income * 0.05
+}
+
+calculate_tax.employee <- function(person, income) {
+  income * 0.18
+}
+
+calculate_tax.business <- function(person, income) {
+  income * 0.25
+}
